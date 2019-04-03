91e4d5ee80a70104ed398bcf98218d241994de7d
May Hnin Phyu <may.hninphyu@sca.com.au>
2019-04-03 15:53:18 +1100

    Tutorial init setup

---
+++ quickstart/cms/api/ingredient/controllers/Ingredient.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Ingredient.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Ingredient`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve ingredient records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    if (ctx.query._q) {
+      return strapi.services.ingredient.search(ctx.query);
+    } else {
+      return strapi.services.ingredient.fetchAll(ctx.query);
+    }
+  },
+
+  /**
+   * Retrieve a ingredient record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    return strapi.services.ingredient.fetch(ctx.params);
+  },
+
+  /**
+   * Count ingredient records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.ingredient.count(ctx.query);
+  },
+
+  /**
+   * Create a/an ingredient record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.ingredient.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an ingredient record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.ingredient.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an ingredient record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.ingredient.remove(ctx.params);
+  }
+};
